29512eb962bcfbdd555b35717ccba32d05d0ff6f
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-12 09:29:15 +0700

    code - safety measure make only Tegao package able to get result from this service

--- app/src/main/java/com/tegaoteam/addon/tegao/handwritingrecognition/RecognitionService.kt
+++ app/src/main/java/com/tegaoteam/addon/tegao/handwritingrecognition/RecognitionService.kt
@@ -5,9 +5,20 @@ import android.content.Intent
 import android.os.IBinder
 
 class RecognitionService : Service() {
+    private var trustedUid: Int? = null
+    private var trustedPackageName = "com.tegaoteam.application.tegao"
+    private fun firstVerify(callingUid: Int) {
+        val callerPackages = packageManager.getPackagesForUid(callingUid)
+        val callerPackageName = callerPackages?.firstOrNull()
+        trustedUid = if (callerPackageName == trustedPackageName) callingUid else -1
+    }
 
     private val binder = object: IRecognitionService.Stub() {
         override fun requestInputSuggestions(input: ByteArray?): List<String?>? {
+            val callingUid = getCallingUid()
+            if (trustedUid == null) firstVerify(callingUid)
+            if (callingUid != trustedUid) return null
+
             val test = RecognitionModel.getSomeRandomChars().apply {
                 add(input?.size.toString())
             }
